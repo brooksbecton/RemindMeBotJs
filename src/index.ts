54da89432b9771c40c8db9aaa1add8b4f32354a3
Brooks Becton <broabect@ut.utm.edu>
2018-08-02 07:33:23 -0500

    Change output to dist. Coment out Reminder model

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as Discord from "discord.js";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import Reminder from "./models/Reminder";
+// import Reminder from "./models/Reminder";
 import createTimeObject from "./common/createTimeObject/index";
 
 import { botHook } from "./constants/bot";
@@ -15,7 +15,7 @@ client.on("ready", () => {
 client.on("message", msg => {
   if (msg.content.toLowerCase().indexOf(botHook) !== -1) {
     const time = createTimeObject(msg.content);
-    Reminder.create({ ...time, msg: "Hey There" });
+    // Reminder.create({ ...time, msg: "Hey There" });
     msg.reply(JSON.stringify(time));
   }
 });
